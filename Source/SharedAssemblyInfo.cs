399e3a95ccc1f700afd46c7b02657c43ce615c27
jeffdarchuk <jdarchuk@connectivedx.com>
2015-07-26 16:27:39 -0700

    restructuring a few things like assembly info, dependencies, and renaming

---
+++ Source/SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Sitecore rich text token management")]
+[assembly: AssemblyCompany("Connective DX")]
+[assembly: AssemblyProduct("TokenManager")]
+[assembly: AssemblyCopyright("Copyright Jeff Darchuk, Connective DX")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
